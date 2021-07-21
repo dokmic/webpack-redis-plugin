0b6c85d1628180259d6de3db64daed2f3634d21b
Michael Dokolin <m@dokol.in>
2021-07-21 20:51:53 +0200

    Add integration tests

---
+++ tests/webpack2/webpack.config.js
@@ -0,0 +1,16 @@
+const WebpackRedisPlugin = require('webpack-redis-plugin');
+
+module.exports = {
+  entry: '../index.js',
+  output: {
+    filename: 'dist/[name].js',
+  },
+  plugins: [
+    new WebpackRedisPlugin({
+      transform: (key, asset) => ({
+        key: 'webpack-redis-plugin',
+        value: asset.source(),
+      }),
+    }),
+  ],
+};
---
+++ tests/webpack4/webpack.config.js
@@ -0,0 +1,14 @@
+const WebpackRedisPlugin = require('webpack-redis-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: '../index.js',
+  plugins: [
+    new WebpackRedisPlugin({
+      transform: (key, asset) => ({
+        key: 'webpack-redis-plugin',
+        value: asset.source(),
+      }),
+    }),
+  ],
+};
